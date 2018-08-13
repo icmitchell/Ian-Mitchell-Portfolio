64df3b02e9aca3a7b50d86c5952bcba6bddc0e81
DESKTOP-05OE308\icmitchell <icmitchell73@yahoo.com>
2018-08-13 13:16:14 -0500

    added resume

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-    $(".img").mouseenter(function() {
-      $(this).addClass("hover");
-    })
-  
-    .mouseleave(function() {
-      $(this).removeClass("hover");
-    });
-  
+	$(".img").mouseenter(function() {
+		$(this).addClass("hover");
+	})
 
-  });
+	.mouseleave(function() {
+		$(this).removeClass("hover");
+	});
+
+
+});
+
+
+$(document).ready(function() {
+	$(".resumeClass").on("click", function() {
+		window.open("assets/Ian-Mitchell.pdf", '_blank');
+	})
+})
